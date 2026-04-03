afbf8171f88e92c707964cea26ead2a02d54bf7b
vlad-yarko <vladyarik88888@gmail.com>
2026-04-03 21:16:45 +0300

    studying

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
 
-data = {
-    'Income': [15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-    'Spending': [39, 81, 6, 77, 40, 76, 6, 94, 3, 72]
-}
-df = pd.DataFrame(data)
 
-kmeans = KMeans(n_clusters=2, random_state=0)
-kmeans.fit(df)
-print(kmeans.labels_)
+
+X, y = make_classification(n_samples=1000, n_features=20, random_state=42)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+
+y_prob = model.predict_proba(X_test)[:, 1]
+fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+roc_auc = auc(fpr, tpr)
+
+
+plt.figure()
+plt.plot(fpr, tpr, label=f'ROC-крива (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC-крива')
+plt.legend()
+plt.show()
